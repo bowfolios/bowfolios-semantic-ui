4f0cf5af48bad33aeba26667f4d6bc2c5293f63c
Philip Johnson <johnson@hawaii.edu>
2020-03-25 13:12:15 -1000

    Add assets-based loading

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -57,3 +57,19 @@ if (Meteor.users.find().count() === 0) {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
 }
+
+/**
+ * If the loadAssetsFile field in settings.development.json is true, then load the data in private/data.json.
+ * This approach allows you to initialize your system with large amounts of data.
+ * Note that settings.development.json is limited to 64,000 characters.
+ * We use the "Assets" capability in Meteor.
+ * For more info on assets, see https://docs.meteor.com/api/assets.html
+ * User count check is to make sure we don't load the file twice, which would generate errors due to duplicate info.
+ */
+if ((Meteor.settings.loadAssetsFile) && (Meteor.users.find().count() < 7)) {
+  const assetsFileName = 'data.json';
+  console.log(`Loading data from private/${assetsFileName}`);
+  const jsonData = JSON.parse(Assets.getText(assetsFileName));
+  jsonData.profiles.map(profile => addProfile(profile));
+  jsonData.projects.map(project => addProject(project));
+}
