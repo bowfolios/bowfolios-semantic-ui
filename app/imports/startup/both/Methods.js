1f99d01072c448853d3fd663e4c77605f5797238
Philip Johnson <johnson@hawaii.edu>
2020-10-23 13:45:02 -1000

    Indicate that insecure mode is not actuallly necessary.

--- app/imports/startup/both/Methods.js
+++ app/imports/startup/both/Methods.js
@@ -8,11 +8,13 @@ import { ProjectsInterests } from '../../api/projects/ProjectsInterests';
 /**
  * In Bowfolios, insecure mode is enabled, so it is possible to update the server's Mongo database by making
  * changes to the client MiniMongo DB.
+ *
  * However, updating the database via client-side calls can be inconvenient for two reasons:
  *   1. If we want to update multiple collections, we need to use nested callbacks in order to trap errors, leading to
  *      the dreaded "callback hell".
  *   2. For update and removal, we can only provide a docID as the selector on the client-side, making bulk deletes
  *      hard to do via nested callbacks.
+ *
  * A simple solution to this is to use Meteor Methods (https://guide.meteor.com/methods.html). By defining and
  * calling a Meteor Method, we can specify code to be run on the server-side but invoked by clients. We don't need
  * to use callbacks, because any errors are thrown and sent back to the client. Also, the restrictions on the selectors
@@ -20,7 +22,8 @@ import { ProjectsInterests } from '../../api/projects/ProjectsInterests';
  *
  * Meteor Methods are commonly introduced as the necessary approach to updating the DB once the insecure package is
  * removed, and that is definitely true, but Bowfolios illustrates that they can simplify your code significantly
- * even when prototyping and using insecure mode.
+ * even when prototyping. It turns out that we can remove insecure mode if we want, as we use Meteor methods to update
+ * the database.
  *
  * Note that it would be even better if each method was wrapped in a transaction so that the database would be rolled
  * back if any of the intermediate updates failed. Left as an exercise to the reader.
