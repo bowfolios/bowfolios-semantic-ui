91ac1017eba445f26c992563c60611f53d3684fd
Philip Johnson <johnson@hawaii.edu>
2020-10-25 14:55:44 -1000

    remove home page warning

--- app/tests/home.page.js
+++ app/tests/home.page.js
@@ -30,9 +30,22 @@ class HomePage {
   async updateProfile(testController, firstName) {
     const newFirstName = 'New First Name';
     await this.isDisplayed(testController);
-    await this.setFirstName(testController, newFirstName);
+    // Delete text from first name field.
+    await testController.selectText('#firstName').pressKey('delete');
+    // Type in new first name.
+    await testController.typeText('#firstName', newFirstName);
+    // Submit it.
+    await testController.click('#home-page-submit');
+    // Click the OK button on the Sweet Alert.
+    await testController.click(Selector('.swal-button--confirm'));
+    // Check that the field is updated.
+    await testController.expect(Selector('#firstName').value).eql(newFirstName);
     // Now restore original value.
-    await this.setFirstName(testController, firstName);
+    await testController.selectText('#firstName').pressKey('delete');
+    await testController.typeText('#firstName', firstName);
+    await testController.click('#home-page-submit');
+    await testController.click(Selector('.swal-button--confirm'));
+    await testController.expect(Selector('#firstName').value).eql(firstName);
   }
 }
 
